fbdef1af8908b67531fc2aa7ca990009bd3e57a1
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2023-06-04 22:07:26 +0600

    Authentication Done

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $req)
+    {
+        try {
+            $validator = Validator::make($req->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 401);
+            }
+            $user = new User();
+            $user->name = $req->name;
+            $user->email = $req->email;
+            $user->password = bcrypt($req->password);
+            $user->address = $req->address;
+            $user->save();
+
+            return response()->json([
+                'success' => true,
+                'data' => $user
+            ], 201);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => "error",
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    public function login(Request $req)
+    {
+
+        try {
+            $validator = Validator::make($req->all(), [
+                'email' => 'required|string|email',
+                'password' => 'required|string|min:6',
+            ]);
+            $token = null;
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 402);
+            }
+            $credentials = $req->only('email', 'password');
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Invalid Email or Password',
+                ], 401);
+            }
+            $user = Auth::user();
+            return response()->json([
+                'success' => true,
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => "error",
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    public function logout()
+    {
+        try {
+            auth()->logout();
+            return response()->json(['message' => 'User successfully signed out']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to logout', 'error' => $e], 500);
+        }
+    }
+
+    public function refresh()
+    {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
+}
---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $data =  Category::all();
+            return response()->json([
+                'data' => $data,
+                'status' => "ok"
+            ], 200);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => "error",
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        try {
+            $data = Category::create($request->all());
+            return response()->json($data = [
+                'message' => "Insert Succesfully",
+                'data' => $data,
+
+            ], 201);
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => "error",
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        try {
+            $data =  Category::find($category->id);
+            if ($data->exists) {
+                return response()->json(['status' => 'ok', 'data' => $data], 200);
+            } else {
+                return response()->json(['status' => 'error', 'message' => 'Resource not found'], 404);
+            }
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => "error",
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        try {
+            $data =  Category::find($category->id);
+            if ($data->exists) {
+                $data->update($request->all());
+                return response()->json(['status' => 'ok', 'data' => $data], 200);
+            } else {
+                return response()->json(['status' => 'error', 'message' => 'Resource not found'], 404);
+            }
+        } catch (\Exception $error) {
+            return response()->json([
+                'message' => "error",
+                'error' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $data =  Category::find($category->id);
+        if ($data->exists) {
+            $data = category::destroy($category->id);
+            return  response()->json(['status' => 'Ok', 'message' => 'Deleted Sucessfully'], 204);
+        } else {
+
+            return response()->json(['status' => 'error', 'message' => 'Resource not found'], 404);
+        }
+    }
+}
