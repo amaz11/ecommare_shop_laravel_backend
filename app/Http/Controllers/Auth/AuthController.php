1a0017cdc751963826732b91dd47cd26a5b444e3
K M Amaz Uddin Shaon <k.m.shaonred@gmail.com>
2023-06-10 09:41:08 +0600

    Jwt Middleware Update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class AuthController extends Controller
 {
--- app/Http/Middleware/JwtCheck.php
+++ app/Http/Middleware/JwtCheck.php
@@ -3,8 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JwtCheck
@@ -16,7 +20,18 @@ class JwtCheck
      */
     public function handle(Request $request, Closure $next): Response
     {
+        try {
         JWTAuth::parseToken()->authenticate();
+        }
+        catch (TokenExpiredException $e) {
+            return response()->json(['message' => 'Token expired'], Response::HTTP_UNAUTHORIZED);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['message' => 'Token invalid'], Response::HTTP_UNAUTHORIZED);
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Token absent'], Response::HTTP_UNAUTHORIZED);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+        }
         return $next($request);
     }
 }
